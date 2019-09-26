683a2d7cafb6e9c557a50da185769dd8466af24d
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2019-09-26 15:08:43 +0200

    Added copyrights header

--- oscm-common/src/main/java/org/oscm/identity/model/TokenDetails.java
+++ oscm-common/src/main/java/org/oscm/identity/model/TokenDetails.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ *
+ *  Copyright FUJITSU LIMITED 2019
+ *
+ *  Creation Date: 25.09.2019
+ *
+ *******************************************************************************/
 package org.oscm.identity.model;
 
 import lombok.Data;
@@ -5,6 +12,6 @@ import lombok.Data;
 @Data
 public class TokenDetails {
 
-    private String token;
-    private String tokenType;
+  private String token;
+  private String tokenType;
 }
