6f65bdb3b60f02b9758e78d6357578b0a8ff61d7
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-10-17 08:06:12 +0200

    Refactored IdentityClient#createGroup to work in create-or-get manner

--- oscm-common/src/main/java/org/oscm/identity/model/ErrorInfo.java
+++ oscm-common/src/main/java/org/oscm/identity/model/ErrorInfo.java
@@ -1,16 +1,20 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  *
- *  Copyright FUJITSU LIMITED 2019
+ * <p>Copyright FUJITSU LIMITED 2019
  *
- *  Creation Date: 20.09.2019
+ * <p>Creation Date: 20.09.2019
  *
- *******************************************************************************/
+ * <p>*****************************************************************************
+ */
 package org.oscm.identity.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 /** Simple data transfer object representing error information */
 @Data
+@Builder(builderMethodName = "of")
 public class ErrorInfo {
 
   private String error;
--- oscm-common/src/main/java/org/oscm/identity/model/GroupInfo.java
+++ oscm-common/src/main/java/org/oscm/identity/model/GroupInfo.java
@@ -1,16 +1,20 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  *
- *  Copyright FUJITSU LIMITED 2019
+ * <p>Copyright FUJITSU LIMITED 2019
  *
- *  Creation Date: 24.09.2019
+ * <p>Creation Date: 24.09.2019
  *
- *******************************************************************************/
+ * <p>*****************************************************************************
+ */
 package org.oscm.identity.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 /** Simple data transfer object representing group information */
 @Data
+@Builder(builderMethodName = "of")
 public class GroupInfo {
 
   private String id;
